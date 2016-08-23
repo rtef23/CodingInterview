7f70d63ce9fceb4715a6b07d0b4bcfbb6bc1b1b8
rtef23 <rtef23@gmail.com>
2016-08-23 13:32:30 +0900

    Create, p92, Q)1.2

---
+++ Array&String/Solutions/ReverseString.java
@@ -0,0 +1,23 @@
+package Solutions;
+
+import java.util.Scanner;
+
+public class ReverseString {
+	/*
+	 * 92p, Q)1.2
+	 * return reverse the string which is ended with null(\o)
+	 * */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("enter the string which you want to reverse : ");
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println(reverseString(scan.next()));
+	}
+	private static String reverseString(String str){
+		String result = new String();
+		for(int i = str.length() - 1;i >= 0;i--)
+			result += str.charAt(i);
+		return result;
+	}
+}
